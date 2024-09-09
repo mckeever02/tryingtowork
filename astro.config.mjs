a6a746ec838bc812678e140741798ea6dd9a5158
Michael McKeever <mckeever02@gmail.com>
2024-09-09 23:04:15 +0100

    Add google places

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    define: {
+          'import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY': JSON.stringify('AIzaSyBlz0UsXYm_NrCoKDjrHxlr9WX5OLB5u-4')
+    }
+  }
+});
