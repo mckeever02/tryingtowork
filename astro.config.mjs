5b876702163bbabe889acc517aa870993bfaa79d
Michael McKeever <mckeever02@gmail.com>
2024-09-11 23:19:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ export default defineConfig({
   integrations: [react(), tailwind()],
   vite: {
     define: {
-      'import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY': JSON.stringify('AIzaSyBlz0UsXYm_NrCoKDjrHxlr9WX5OLB5u-4'),
-      'import.meta.env.SUPABASE_URL': JSON.stringify('https://uqsorrbgasntikjgfokx.supabase.co'),
-      'import.meta.env.SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxc29ycmJnYXNudGlramdmb2t4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjA5MjI0OSwiZXhwIjoyMDQxNjY4MjQ5fQ.2yTv5iuKITB80rdbYXOaT84B7tzVCCvmky6p0WhpwVE')
+      'import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.PUBLIC_GOOGLE_MAPS_API_KEY),
+      'import.meta.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+      'import.meta.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY)
     },
     plugins: []  // Ensure this is empty or remove it if you haven't added any Vite plugins
   }
