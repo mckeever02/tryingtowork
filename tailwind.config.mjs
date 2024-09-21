cfd4815461becc586e6a5cb3dc5f7707d4c1a58e
Michael McKeever <mckeever02@gmail.com>
2024-09-21 21:45:39 +0100

    Add tabs animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			transitionTimingFunction: {
+				'spring-soft': 'cubic-bezier(0.5, 0, 0.5, 1.5)',
+				'spring-medium': 'cubic-bezier(0.5, 0, 0.1, 1.5)',
+				'spring-hard': 'cubic-bezier(0.5, -0.5, 0.1, 1.5)',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss/plugin')(({ addUtilities }) => {
+			addUtilities({
+				'.transition-spring-soft': {
+					'transition-timing-function': 'cubic-bezier(0.5, 0, 0.5, 1.5)',
+				},
+				'.transition-spring-medium': {
+					'transition-timing-function': 'cubic-bezier(0.5, 0, 0.1, 1.5)',
+				},
+				'.transition-spring-hard': {
+					'transition-timing-function': 'cubic-bezier(0.5, -0.5, 0.1, 1.5)',
+				},
+			})
+		}),
+	],
 }
